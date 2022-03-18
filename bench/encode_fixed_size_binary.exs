fe8f2ee90f2bcd4362b01ef325b64c2444ce91ad
xinz <joy2everyone.z@gmail.com>
2022-03-18 15:42:22 +0800

    Fix bench of commit 609b597

--- bench/encode_fixed_size_binary.exs
+++ bench/encode_fixed_size_binary.exs
@@ -1,7 +1,15 @@
+defmodule Bench.Bits8 do
+  use CrockfordBase32, bits_size: 8
+end
+
+defmodule Bench.Bits128 do
+  use CrockfordBase32, bits_size: 128
+end
+
 Benchee.run(
   %{
     "common encode small string" => fn -> CrockfordBase32.encode("b") end,
-    "fix size to encode small string" => fn -> A.encode("b") end
+    "fix size to encode small string" => fn -> Bench.Bits8.encode("b") end
   }
 )
 
@@ -10,6 +18,6 @@ data = <<System.system_time(:millisecond)::unsigned-size(48), :crypto.strong_ran
 Benchee.run(
   %{
     "common encode 128 bits string" => fn -> CrockfordBase32.encode(data) end,
-    "fix size to encode 128 bits string" => fn -> BitsData128.encode(data) end
+    "fix size to encode 128 bits string" => fn -> Bench.Bits128.encode(data) end
   }
 )
