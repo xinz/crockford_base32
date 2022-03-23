1b6f1dcff57a91ecf989873c9f4cad3958c394fc
xinz <joy2everyone.z@gmail.com>
2022-03-23 15:54:37 +0800

    Update bench

---
+++ bench/decode_string.exs
@@ -0,0 +1,12 @@
+defmodule Fixed128 do
+  use CrockfordBase32,
+    bits_size: 128
+end
+
+Benchee.run(
+  %{
+    "decode_to_binary" => fn -> CrockfordBase32.decode_to_binary("05ZV1G43W81HS41V10MT4Z1WMC", []) end,
+    "fixed" => fn -> Fixed128.decode("05ZV1G43W81HS41V10MT4Z1WMC") end
+  },
+  print: [fast_warning: false]
+)
