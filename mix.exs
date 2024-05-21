7c2ec0b30790bca2672fc59b7f048f6da293939c
xinz <joy2everyone.z@gmail.com>
2024-05-21 20:30:24 +0800

    Version 0.7.1

--- mix.exs
+++ mix.exs
@@ -6,7 +6,7 @@ defmodule CrockfordBase32.MixProject do
   def project do
     [
       app: :crockford_base32,
-      version: "0.7.0",
+      version: "0.7.1",
       elixir: "~> 1.7",
       start_permanent: Mix.env() == :prod,
       deps: deps(),
