b8a65102175ab93299588430e150391e5633c351
xinz <joy2everyone.z@gmail.com>
2024-05-21 20:23:43 +0800

    Fix naming in bench

--- bench/decode_string.exs
+++ bench/decode_string.exs
@@ -5,7 +5,7 @@ end
 
 Benchee.run(
   %{
-    "decode_to_binary" => fn -> CrockfordBase32.decode_to_binary("05ZV1G43W81HS41V10MT4Z1WMC", []) end,
+    "decode_to_binary" => fn -> CrockfordBase32.decode_to_bitstring("05ZV1G43W81HS41V10MT4Z1WMC", []) end,
     "fixed" => fn -> Fixed128.decode("05ZV1G43W81HS41V10MT4Z1WMC") end
   },
   print: [fast_warning: false]
